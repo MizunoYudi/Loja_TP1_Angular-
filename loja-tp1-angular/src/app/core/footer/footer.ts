7d58bec850799bccbd8a13e61ed39816c329664a
MizunoYudi <joaomizunovy@gmail.com>
2025-08-31 18:02:13 -0300

    Criando Footer e Banner

---
+++ loja-tp1-angular/src/app/core/footer/footer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'a-footer',
+  imports: [],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css'
+})
+export class Footer {
+
+}
