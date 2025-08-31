4dc180cd60c4958d1e29ecee6c77114f54a2cbe4
MizunoYudi <joaomizunovy@gmail.com>
2025-08-31 17:43:51 -0300

    Criando Header

--- loja-tp1-angular/src/app/core/header/header.ts
+++ loja-tp1-angular/src/app/core/header/header.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'a-header',
   imports: [],
   templateUrl: './header.html',
   styleUrl: './header.css'
