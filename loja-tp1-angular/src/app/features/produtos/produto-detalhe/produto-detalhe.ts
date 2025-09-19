be02eb24580985ed71845670f333b5390bad291e
MizunoYudi <joaomizunovy@gmail.com>
2025-09-19 20:35:19 -0300

    Criando Produto Detalhe

--- loja-tp1-angular/src/app/features/produtos/produto-detalhe/produto-detalhe.ts
+++ loja-tp1-angular/src/app/features/produtos/produto-detalhe/produto-detalhe.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProdutoService } from '../services/produto.service';
+import { Produto } from '../../../model/produto';
 
 @Component({
   selector: 'app-produto-detalhe',
@@ -12,4 +13,27 @@ export class ProdutoDetalhe {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   private produtoService = inject(ProdutoService);
+
+  loading = signal(true);
+  produto = signal<Produto | undefined>(undefined);
+
+  constructor() {
+    this.route.paramMap.subscribe(pm => {
+      const id = pm.get('id') ? Number(pm.get('id')) : NaN;
+      if(isNaN(id)){
+        this.produto.set(undefined);
+        this.loading.set(false);
+        return;
+      }
+      this.loading.set(true);
+      this.produtoService.getById(id).subscribe(p => {
+        this.produto.set(p);
+        this.loading.set(false);
+      });
+    })
+  }
+
+  voltar(){
+    this.router.navigate(['/produtos']);
+  }
 }
