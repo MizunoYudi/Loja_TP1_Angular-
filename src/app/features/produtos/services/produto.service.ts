b72b66e39133fe0ca82e0f626585fd2d5c652cb0
MizunoYudi <joaomizunovy@gmail.com>
2025-11-14 20:27:17 -0300

    Adicionando função criarProduto

--- src/app/features/produtos/services/produto.service.ts
+++ src/app/features/produtos/services/produto.service.ts
@@ -9,18 +9,31 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProdutoService {
   logger = inject(LoggerService);
-  client = inject(HttpClient);
+  http = inject(HttpClient);
+  apiURL = 'https://fakestoreapi.com/products';
 
   listar(): Observable<Produto[]> {
     this.logger.info("ProdutoService - Listando produtos");
-    return this.client.get<any[]>('https://fakestoreapi.com/products').pipe(
+    return this.http.get<any[]>(this.apiURL).pipe(
       map(lista => lista.map((json) => ProdutoMapper.fromJson(json))),
       catchError(err => of([]))
     );
   }
 
-  getById(id: number): Observable<Produto | undefined>{
+  getById(id: number): Observable<Produto | undefined> {
     return of(); //Exercicio -> 
     //this.listaMock.find(p => p.id = id)).pipe(delay(500)
+  }
+
+  criarProduto(produto: Produto): Observable<any> {
+    let body = {
+      'title': produto.nome,
+      'price': produto.preco,
+      'description': produto.descricao,
+      'image': produto.imageUrl,
+      'category': produto.categoria
+    }
+
+    return this.http.post(this.apiURL, body);
   }
 }
